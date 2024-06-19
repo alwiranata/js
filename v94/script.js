e7bbbdc1750dc8fc2b70e4e24487f425a8fee217
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-06-19 09:44:29 +0700

    update

---
+++ v94/script.js
@@ -0,0 +1,26 @@
+let colors = ["red", " green", " blue"]
+console.log(colors)
+colors.push("yellow")
+console.log(colors)
+console.log(colors.length)
+colors = []
+console.log(colors)
+// console.log(colors.length)
+
+let numbers = [10, , 20, 30]
+console.log(numbers.length)
+
+numbers[10] = 100
+console.log(numbers)
+console.log(numbers.length)
+
+const fruits = ["Apple", " Orange", "Strawbery"]
+fruits.length = 0
+console.log(fruits)
+
+const fruits2 = ["Banana", "Apple", "WaterMelon"]
+fruits2.length = 3
+console.log(fruits2)
+console.log(fruits2.length)
+fruits2.length = 4
+console.log(fruits2)
