6ec87e4210b7b9c68903620537484fdc15ce7498
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-06-23 20:53:44 +0700

    update

---
+++ v99/script.js
@@ -0,0 +1,10 @@
+let scorres = [1, 2, 3, 4, 5]
+let deletescores = scorres.splice(3, 1)
+console.log(deletescores)
+console.log(scorres)
+
+let lenguage = ["CSS", "JS", "PYTHON"]
+lenguage.splice(1, 0, "HTML", "PW")
+console.log(lenguage)
+lenguage.splice(1, 0, "EMAIL")
+console.log(lenguage)
