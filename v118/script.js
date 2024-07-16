bd3e90ceab84e949f507ec031c9354022da2b039
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-07-16 13:21:09 +0700

    update

---
+++ v118/script.js
@@ -0,0 +1,7 @@
+const cssClasss = ["btn-outline", "btn-primary", "btn-outeside"]
+const css = cssClasss.join(" ")
+console.log(css)
+
+const title = "Javascript Array Join Example"
+const add = title.split(" ").join("-").toLowerCase()
+console.log(add)
