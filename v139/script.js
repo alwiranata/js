14e4f161579168296bc4d48ed069d88e4d4a431c
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-08-04 17:22:12 +0700

    update

--- v139/script.js
+++ v139/script.js
@@ -7,3 +7,15 @@ let input2 = document.querySelector("#username")
 input2.setAttribute("placeholder", "Masukkan Username : ")
 input2.setAttribute("tabindex", 2)
 console.log(input2.getAttribute("tabindex"))
+
+input2.setAttribute("value", "guest")
+console.log(input2.value)
+
+input2.value = "admin"
+console.log(input2.getAttribute("value"))
+
+let checkbox = document.querySelector("#checkbox")
+console.log(checkbox.checked)
+
+let password = document.querySelector("#password")
+console.log(password.getAttribute("style"))
